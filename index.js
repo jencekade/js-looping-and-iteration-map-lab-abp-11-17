592bfeea8d630255042f59923b3083292a5bbeb4
jencekade <gaephuoc@gmail.com>
2017-11-08 10:40:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 // Code your solution in this file.
-function lowerCaseDrivers() {
-  drivers.map(function (drivers){
-    let loweredCaseDrivers = drivers.toLowerCase();
-    return loweredCaseDrivers;
+function lowerCaseDrivers(drivers) {
+  let loweredCaseDrivers1 = drivers.map(function(eachDrivers) {
+    return eachDrivers.toLowerCase();
   });
-  return drivers;
+  return loweredCaseDrivers1;
 }
 
-function nameToAttributes() {
+function nameToAttributes(drivers) {
+  let namedToAtrributes1 = drivers.map(function (eachDrivers){
 
+  });
+  return namedToAttributes1;
 }
 
-function attributesToPhrase() {
-
+function attributesToPhrase(drivers) {
+  let attributedToPhrase1 = drivers.map(function (eachDriver) {
+    return `${eachDriver.name} is from ${eachDriver.hometown}`;
+  });
+  return attributedToPhrase1;
 }

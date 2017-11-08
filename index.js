351a070071a921b2700bf2f90c90b8b4f5f38264
jencekade <gaephuoc@gmail.com>
2017-11-08 10:08:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file.
+function lowerCaseDrivers() {
+  drivers.map(function (drivers){
+    let loweredCaseDrivers = drivers.toLowerCase();
+    return loweredCaseDrivers;
+  });
+  return drivers;
+}
+
+function nameToAttributes() {
+
+}
+
+function attributesToPhrase() {
+
+}
